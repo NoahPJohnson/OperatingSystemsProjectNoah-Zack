6d7d6f289e3e8bf135b99dfd565275733c15bb9e
CSIS Student <dreamedlint@gmail.com>
2017-03-19 22:14:11 -0400

    Committing semi-working vdi Unix Calls cpp file

--- Project/vdi_UnixCalls.cpp
+++ Project/vdi_UnixCalls.cpp
@@ -58,9 +58,12 @@ translate()
 
 */
 
-#include <vdiUnixCalls.h>
+#include "vdiUnixCalls.h"
 #include <sys/types.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 //VDI Header
@@ -68,8 +71,13 @@ struct vdiHeader
 {
     char name[64];
     unsigned int magicNumber;
+    unsigned int offsetBlocks; //how far into file the map is
+    unsigned int offsetData;   //first page frame location
+    unsigned int blockSize;    //page size
+    unsigned int diskSize;     //total size of virtual drive
     unsigned short int majorVersion;
     unsigned short int minorVersion;
+    
     //Ignore UUID's   
 };
 
@@ -77,28 +85,51 @@ class vdiFile
 {
     int fd;
     int cursor;
+    vdiHeader header;
+    
     
     public:
         int vdi_open(const char*);
-        void vdi_close(int);
-        ssize_t vdi_read(int, void*, size_t);
+        void vdi_close();
+        ssize_t vdi_read(/*int,*/ void*, size_t);
         ssize_t vdi_write(int, const void*, size_t);
-        off_t vdi_lseek(int, off_t, int);
+        off_t vdi_lseek(off_t, int);
+        void setHeader();
         void translate();
         
 };
 
-int vdiFile::vdi_open(const char pathname*)
+int main()
+{
+    //fstream fs;
+    //fs.open ("Test-fixed-1k.vdi", fstream::in);
+    
+    //cout << "FILE CONTENTS:  " << fs.read(); 
+    //fs.close();
+    char data[2560];
+
+    vdiFile testFile;    
+
+    testFile.vdi_open("/home/csis/Downloads/Good/Test-fixed-1k.vdi");
+    testFile.vdi_lseek(5, SEEK_SET);
+    testFile.vdi_read(data, 1920);
+    testFile.vdi_close();
+    cout << data << endl;
+    return 0;
+}
+
+int vdiFile::vdi_open(const char *pathname)
 {
     //int fd;
     //int cursor;
-    fd = open(fn, O_RDONLY);
+    fd = open(pathname, O_RDONLY);
+    cout << fd << endl;
     if (fd < 0)
     {
         return 1;
     }
     // read header
-    read(fd, )    
+    //read(fd, )    
     //read map
     
     cursor = 0;
@@ -110,28 +141,41 @@ void vdiFile::vdi_close()
     close(fd);
 }
         
-ssize_t vdi_read(int fd, void *buf, size_t count)
+ssize_t vdiFile::vdi_read(/*const char *fn,*/ /*int fd,*/ void *buf, size_t count)
 {
+    //fd = file discriptor, read this.
+    //*buf = buffer what is read goes here.
+    //count = how far to read into file.
     //info = read(fd)
+    //fd = open(*fn /*, O_RDONLY*/);
+    //lseek the starting point
+    read(fd, buf, count);
 }        
 
-ssize_t vdi_write(int fd, const void *buf, size_t count)
+ssize_t vdiFile::vdi_write(int fd, const void *buf, size_t count)
 {
     //write(fd)
+    //write(fd, *buf, count);
 }
 
-off_t vdi_lseek(int fildes, off_t offset, int whence)
+off_t vdiFile::vdi_lseek(off_t offset, int whence)
 {
-
+    lseek(fd, offset, whence);
 }
 
-void translate()
+void vdiFile::setHeader()
+{
+    header.offsetBlocks = 0;
+}
+
+void vdiFile::translate()
 {
     int vdiPageSize;
     int pageNumber;
     int offset;
-    int[] map;
-    cursor / vdiPageSize = pageNumber;
-    cursor % vdiPageSize = offset;
-    map[pageNumber] * vdiPageSize + offset;   
+    //int[] map;
+    //map = int[vdiPageSize];
+    //cursor / vdiPageSize = pageNumber;
+    //cursor % vdiPageSize = offset;
+    //map[pageNumber] * vdiPageSize + offset;   
 }
